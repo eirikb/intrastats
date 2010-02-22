e4fd8a343c761f283506c09bb423b1287e4481fa
eirikb@heavyload <get_email eirikb@heavyload>
2010-02-22 16:07:32 +0100

    Støtte for seksjon i databasen

--- IntraStats/grails-app/domain/no/forsvaret/intrastats/Page.groovy
+++ IntraStats/grails-app/domain/no/forsvaret/intrastats/Page.groovy
@@ -3,13 +3,16 @@ package no.forsvaret.intrastats
 class Page {
 
     static hasMany = [visits : Visit]
+    static belongsTo = [section : Section]
 
     String title
     String url
     Date dateCreated
-
+    Section section
+    
     static constraints = {
         title(nullable: true)
         url(nullable: false, unique: true)
+        section(nullable: true)
     }
 }
