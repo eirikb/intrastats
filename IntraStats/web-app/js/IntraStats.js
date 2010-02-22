22b177888a9ddc68469b45d230e34a7997ed1c4a
Sindre <get_email Sindre>
2010-02-22 15:42:33 +0100

    updated js file

--- IntraStats/web-app/js/IntraStats.js
+++ IntraStats/web-app/js/IntraStats.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-    $.extend({
-        getUrlVars: function(){
-            var vars = [], hash;
-            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-            for(var i = 0; i < hashes.length; i++){
-                hash = hashes[i].split('=');
-                vars.push(hash[0]);
-                vars[hash[0]] = hash[1];
-            }
-            return vars;
-        },
-        unparam: function(name){
-            return $.getUrlVars()[name];
-        }
-    });
+$(function() {
+	$.extend({4(window
+		getUrlVars: function(){
+			var vars = [], hash;
+			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+			for(var i = 0; i < hashes.length; i++){
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	},
+		unparam: function(name){
+			return $.getUrlVars()[name];
+		}
+	});
 
-    var articleID = $.unparam('articleID');
+	var articleID = $.unparam('articleID');
 
-    $.getJSON('/pageVisit/index?jsoncallback=?', {
-        url: window.location.href.split(';')[0],
-        articleID: articleID,
-        screenWidth: screen.width,
-        screenHeight: screen.height,
-        referrer: document.referrer.split(';')[0],
-        title: articleID ? $('.artTitle').text() : document.title
-    });
+	$.getJSON('ip:port?jsoncallback=?', {
+		url: window.location.href.split(';')[0],
+		articleID: articleID,
+		browserWidth: $(window).width(),
+		browserHeight: $(window).height(),
+		referrer: document.referrer.split(';')[0],
+		title: articleID ? $('.artTitle').text() : document.title
+	}); 
 });
