516732633a184784557cfd7e3d55c7a955fcce24
eirikb@katze <get_email eirikb@katze>
2010-02-24 22:15:00 +0100

    Fiksa PageVisitController til å fungere uten request! BootStrap tar utnytte av nye PageVisitController og SPAMMER inn visits (hvis devmode)

--- IntraStats/grails-app/conf/BootStrap.groovy
+++ IntraStats/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,39 @@
+
+import no.forsvaret.intrastats.*
+import grails.util.GrailsUtil
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+
+
+    def init = { servletContext ->
+
+
+
+        switch (GrailsUtil.environment) {
+            case "development":
+            def pv = new PageVisitController()
+            for(i in 0..100) {
+
+                int id = (Math.random() * 3)
+                def section = pv.getSection("Section " + id)
+
+                id = (Math.random() * 10)
+                def page =  pv.getPage("http://www.testpage" + id + ".com", "Testpage " + id)
+
+                id = (Math.random() * 5)
+                def client = pv.getClient("Address " + id, "Host " + id, "User-agent " + id)
+
+                if (section != null && page != null && client != null) {
+                    pv.registerVisit(section, page, client, null, id, id)
+                } else {
+                    println "FRAK!"
+                }
+            }
+            break
+        }
+    }
+    
+    def destroy = {
+    }
 } 
