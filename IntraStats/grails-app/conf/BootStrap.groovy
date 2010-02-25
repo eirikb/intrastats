c175db610bed48e062ef9c260b2dc26c63855bc8
eirikb@katze <get_email eirikb@katze>
2010-02-25 20:58:15 +0100

    Ny og raskere og "rettere" generering av Visits

--- IntraStats/grails-app/conf/BootStrap.groovy
+++ IntraStats/grails-app/conf/BootStrap.groovy
@@ -13,27 +13,51 @@ class BootStrap {
         switch (GrailsUtil.environment) {
             case "development":
             def pv = new PageVisitController()
-            for(i in 0..100) {
 
-                int id = (Math.random() * 3)
-                def section = pv.getSection("Section " + id)
+            def sections = new Section[3]
+            for (i in 0..sections.length - 1) {
+                sections[i] = pv.getSection("Section " + i)
+            }
 
-                id = (Math.random() * 10)
-                def page =  pv.getPage("http://www.testpage" + id + ".com", "Testpage " + id)
+            def pages = new Page[10]
+            for (i in 0..pages.length - 1) {
+                pages[i] =  pv.getPage("http://www.testpage" + i + ".com", "Testpage " + i)
+            }
 
-                id = (Math.random() * 5)
-                def client = pv.getClient("Address " + id, "Host " + id, "User-agent " + id)
+            def clients = new Client[5]
+            for (i in 0..clients.length - 1) {
+                clients[i] = pv.getClient("Address " + i, "Host " + i, "User-agent " + i)
+            }
 
+            def p = 0
+            def total = 100
+            pv.timeOut = 0
+            for(i in 0..total) {
+                def section = sections[(int)(Math.random() * (sections.length - 1))]
+                def page = pages[(int)(Math.random() * (pages.length - 1))]
+                def client = clients[(int)(Math.random() * (clients.length - 1))]
+                
                 if (section != null && page != null && client != null) {
-                    pv.registerVisit(section, page, client, null, id, id)
+                    pv.registerVisit(section, page, client, null, i, i)
                 } else {
                     println "FRAK!"
                 }
+                def p2 = (int)((i / total) * 100)
+                if (p2 != p) {
+                    p = p2
+                    println p + "%"
+                }
             }
+
+            Visit.list().each()  {
+                it.setDateCreated(new Date(System.currentTimeMillis() - (long)(Math.random() * (1000L * 60L * 60L * 24L * 32L))))
+                it.save()
+            }
+            println "Number of visits: " + Visit.count()
             break
         }
     }
     
     def destroy = {
     }
-} 
+}
