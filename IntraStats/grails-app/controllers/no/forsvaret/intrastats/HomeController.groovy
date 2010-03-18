fc2cd0152e05b4e458e6ca35ecf26a52b6c11998
eirikb@katze <get_email eirikb@katze>
2010-03-18 18:04:06 +0100

    Site, section og page - visits / views listes opp på framsia

--- IntraStats/grails-app/controllers/no/forsvaret/intrastats/HomeController.groovy
+++ IntraStats/grails-app/controllers/no/forsvaret/intrastats/HomeController.groovy
@@ -3,6 +3,15 @@ package no.forsvaret.intrastats
 class HomeController {
 
     def index = {
-        [sections : Section.list()]
+        def calendar = new GregorianCalendar()
+        calendar.setTime(new Date())
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        def today = calendar.getTime()
+        def siteVisits = SiteVisitRel.countByDateCreatedGreaterThan(today)
+        def sectionVisits =  SectionVisitRel.countByDateCreatedGreaterThan(today)
+        def pageVisits = PageVisitRel.countByDateCreatedGreaterThan(today)
+        [siteVisits: siteVisits, sectionVisits: sectionVisits, pageVisits: pageVisits, sections : Section.list()]
     }
 }
