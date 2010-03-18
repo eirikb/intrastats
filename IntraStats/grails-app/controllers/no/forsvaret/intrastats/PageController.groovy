d97724057e0707a284f5585d17bd314d18b9ab20
eirikb@katze <get_email eirikb@katze>
2010-03-18 20:13:48 +0100

    Kan nå liste opp Pages. Fjernet ting jeg ikke tror vi trenger (show, edit, delete)

--- IntraStats/grails-app/controllers/no/forsvaret/intrastats/PageController.groovy
+++ IntraStats/grails-app/controllers/no/forsvaret/intrastats/PageController.groovy
@@ -1,9 +1,7 @@
 package no.forsvaret.intrastats
 
 class PageController {
-
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-
+    
     def index = {
         redirect(action: "list", params: params)
     }
@@ -11,97 +9,13 @@ class PageController {
     def list = {
         params.max = Math.min(params.max ? params.int('max') : 25, 100)
         params.offset = params.offset != null ? params.offset : 0
-        def sorting = " order by " + (params.sort != null ? params.sort : "visits.size")
+        def sorting = " order by " + (params.sort != null ? params.sort : " count(pvr)")
         sorting += params.order != null ? " " + params.order : " desc"
-        def section = params.id != null ? " where section = " +params.id : ""
-        def pages = Page.executeQuery("select id, url, title, dateCreated, \
-            visits.size from Page" + section + sorting, [max:params.max, offset:params.offset])
-        [pageInstanceList: pages, pageInstanceTotal: Page.count(),
-            visits:pages]
-    }
-
-    def create = {
-        def pageInstance = new Page()
-        pageInstance.properties = params
-        return [pageInstance: pageInstance]
-    }
-
-    def save = {
-        def pageInstance = new Page(params)
-        if (pageInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'page.label', default: 'Page'), pageInstance.id])}"
-            redirect(action: "show", id: pageInstance.id)
-        }
-        else {
-            render(view: "create", model: [pageInstance: pageInstance])
-        }
-    }
-
-    def show = {
-        def pageInstance = Page.get(params.id)
-        if (!pageInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            [pageInstance: pageInstance]
-        }
-    }
-
-    def edit = {
-        def pageInstance = Page.get(params.id)
-        if (!pageInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            return [pageInstance: pageInstance]
-        }
-    }
-
-    def update = {
-        def pageInstance = Page.get(params.id)
-        if (pageInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (pageInstance.version > version) {
-                    
-                    pageInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'page.label', default: 'Page')] as Object[], "Another user has updated this Page while you were editing")
-                    render(view: "edit", model: [pageInstance: pageInstance])
-                    return
-                }
-            }
-            pageInstance.properties = params
-            if (!pageInstance.hasErrors() && pageInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'page.label', default: 'Page'), pageInstance.id])}"
-                redirect(action: "show", id: pageInstance.id)
-            }
-            else {
-                render(view: "edit", model: [pageInstance: pageInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-            redirect(action: "list")
-        }
-    }
-
-    def delete = {
-        def pageInstance = Page.get(params.id)
-        if (pageInstance) {
-            try {
-                pageInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'page.label', default: 'Page'), params.id])}"
-            redirect(action: "list")
-        }
+        def section = params.id != null ? " where p.section.id = " + params.id : ""
+        def pages = Page.executeQuery("select p.id, p.url, p.title, p.dateCreated, count(pvr)  \
+            from PageVisitRel pvr right join pvr.page p" + section + " group by p.id, p.url, p.title, p.dateCreated" + sorting, [max:params.max, offset:params.offset])
+        def pageCount = params.id != null ? Page.countBySection(Section.get(params.id)) : Page.count()
+        pages.each() { println Page.get(it[0]).section.id}
+        [pageInstanceList: pages, pageInstanceTotal: pageCount]
     }
 }
--- IntraStats/grails-app/controllers/no/forsvaret/intrastats/SectionController.groovy
+++ IntraStats/grails-app/controllers/no/forsvaret/intrastats/SectionController.groovy
@@ -10,28 +10,6 @@ class SectionController {
         redirect(action: "list", params: params)
     }
 
-    def list = {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [sectionInstanceList: Section.list(params), sectionInstanceTotal: Section.count()]
-    }
-
-    def create = {
-        def sectionInstance = new Section()
-        sectionInstance.properties = params
-        return [sectionInstance: sectionInstance]
-    }
-
-    def save = {
-        def sectionInstance = new Section(params)
-        if (sectionInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'section.label', default: 'Section'), sectionInstance.id])}"
-            redirect(action: "show", id: sectionInstance.id)
-        }
-        else {
-            render(view: "create", model: [sectionInstance: sectionInstance])
-        }
-    }
-
     def show = {
         def sectionInstance = Section.get(params.id)
         if (!sectionInstance) {
@@ -40,18 +18,7 @@ class SectionController {
         }
         else {
             def visitCount = null
-            if (params.submit != null) {
-                //TODO: hent ut info.
-                //TODO: Vis info i tabellform + kanskje fusion chart.
-                //TODO: Bruk jQuery datepicker
-            }
-
-            //TODO: List ut alle pages
-            //TODO: Browsertype(user-agent)-statistikk!
-            //TODO: browsersize
-
             def data = getVisits();
-
             [sectionInstance: sectionInstance, visitCount: visitCount]
         }
     }
@@ -74,8 +41,8 @@ class SectionController {
                     toDate = new Date()
                 }
             }
-            def  visitlist = Visit.executeQuery("select v, c from Visit v left join v.page p left join v.client c left join p.section s where s = ? \
-                                                and v.dateCreated >= ? and v.dateCreated <= ?", sectionInstance, fromDate, toDate)
+            def  visitlist = Visit.executeQuery("select v, c from SectionVisitRel svr left join svr.section s left join svr.visit v left join v.client c where s = ? \
+                                                and svr.dateCreated >= ? and svr.dateCreated <= ?", sectionInstance, fromDate, toDate)
             def browsers = ["MSIE 5.0":["Internet Explorer 5.0", 0],
                             "MSIE 5.5":["Internet Explorer 5.5", 0],
                             "MSIE 6.0":["Internet Explorer 6.0", 0],
@@ -135,63 +102,6 @@ class SectionController {
             browserData += "</graph>"
 
             return [visitData: visitData, browserData: browserData, visitCount: visitlist.size()] as JSON
-        }
-    }
-
-    def edit = {
-        def sectionInstance = Section.get(params.id)
-        if (!sectionInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'section.label', default: 'Section'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            return [sectionInstance: sectionInstance]
-        }
-    }
-
-    def update = {
-        def sectionInstance = Section.get(params.id)
-        if (sectionInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (sectionInstance.version > version) {
-                    
-                    sectionInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'section.label', default: 'Section')] as Object[], "Another user has updated this Section while you were editing")
-                    render(view: "edit", model: [sectionInstance: sectionInstance])
-                    return
-                }
-            }
-            sectionInstance.properties = params
-            if (!sectionInstance.hasErrors() && sectionInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'section.label', default: 'Section'), sectionInstance.id])}"
-                redirect(action: "show", id: sectionInstance.id)
-            }
-            else {
-                render(view: "edit", model: [sectionInstance: sectionInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'section.label', default: 'Section'), params.id])}"
-            redirect(action: "list")
-        }
-    }
-
-    def delete = {
-        def sectionInstance = Section.get(params.id)
-        if (sectionInstance) {
-            try {
-                sectionInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'section.label', default: 'Section'), params.id])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'section.label', default: 'Section'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'section.label', default: 'Section'), params.id])}"
-            redirect(action: "list")
         }
     }
 }
