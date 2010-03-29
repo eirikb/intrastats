baba20dd64493d4723255910c770f39beb2132e8
eirikb@heavyload <get_email eirikb@heavyload>
2010-03-29 09:26:42 +0200

    Nytt IntraStats.js og medfølgende eksempel (Example.html). Bruker nå bare én linje med <script... /> for å inkludere all info som trengs (host og section)

--- IntraStats/web-app/js/IntraStats.js
+++ IntraStats/web-app/js/IntraStats.js
@@ -1,38 +1,16 @@
 $(function() {
-    $.extend({
-        getUrlVars: function(){
-            var vars = [], hash;
-            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-            for(var i = 0; i < hashes.length; i++){
-                hash = hashes[i].split('=');
-                vars.push(hash[0]);
-                vars[hash[0]] = hash[1];
-            }
-            return vars;
-        },
-        unparam: function(name){
-            return $.getUrlVars()[name];
-        }
-    });
+    var scriptUrl = $("#intrastats").attr("src");
+  
+    var host = scriptUrl.substring(0, scriptUrl.lastIndexOf('/js/'));
+    host += host.charAt(host.length - 1) == '/' ? "" :  '/';
+    
+    var paramsPos = scriptUrl.indexOf('?');
+    var params = paramsPos >= 0 ? scriptUrl.substring(paramsPos + 1) : "";
 
-    var articleID = $.unparam('articleID');
-
-    if (HOST.charAt(HOST.length - 1) != '/')  {
-        HOST += '/';
-    }
-
-    if(typeof(SECTION) === 'undefined') {
-        SECTION = "";
-    } else {
-        SECTION = "section=" + SECTION + '&';
-    }
-
-    $.getJSON(HOST + 'pageVisit/index?' + SECTION + 'jsoncallback=?', {
+    $.getJSON(host + "pageVisit/index?" + params + "&jsoncallback=?", {
         url: window.location.href.split(';')[0],
-        articleID: articleID,
         browserWidth: $(window).width(),
         browserHeight: $(window).height(),
-        referrer: document.referrer.split(';')[0],
-        title: articleID ? $('.artTitle').text() : document.title
+        title: document.title
     });
 });
